a34c82e5a2b6873ccac82415e2124e4993c1b3fa
AaronAlpha <reachaaronbernard@gmail.com>
2025-12-27 22:40:41 -0700

    Update main_menu.gd

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -15,15 +15,18 @@ func _process(delta: float) -> void:
 
 # start button
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+	get_tree().change_scene_to_file("res://Scenes/testing_scene_node_2d.tscn")
 
 # controls button
 func _on_controls_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+	pass
+	
 
 # settings button
 func _on_settings_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+	pass
 
 # exit button
 func _on_exit_button_pressed() -> void:
