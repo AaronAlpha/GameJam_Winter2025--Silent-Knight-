69d5504ab24a6bd3fe64dc7768fefb191d3fdf13
AaronAlpha <reachaaronbernard@gmail.com>
2025-12-27 21:52:47 -0700

    created main_menu.gd
    this is the script for MainMenu_Scene
    
    connected the buttons (through signals) to the MainMenu_Scene parent node

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# the following are the signals connecting to the 4 buttons created: Start; Controls; Settings; Exit
+
+# start button
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+
+# controls button
+func _on_controls_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+
+# settings button
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
+
+# exit button
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
