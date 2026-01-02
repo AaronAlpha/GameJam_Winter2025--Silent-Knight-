b6c945597969bb5767f13d645579b147754e1182
AaronAlpha <reachaaronbernard@gmail.com>
2026-01-02 05:58:04 -0700

    updates

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -17,17 +17,6 @@ func _process(delta: float) -> void:
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/testing_scene_node_2d.tscn")
 
-# controls button
-func _on_controls_button_pressed() -> void:
-	#get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
-	pass
-	
-
-# settings button
-func _on_settings_button_pressed() -> void:
-	#get_tree().change_scene_to_file("res://Scenes/play_control.tscn")
-	pass
-
 # exit button
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
